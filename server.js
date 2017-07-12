067538c32bc19a1e347387aa4e779710f04207c1
Shery11 <laughnplay>
2017-07-13 02:16:13 +0500

    seperated middleware

--- server.js
+++ server.js
@@ -2,23 +2,15 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication : function(req,res,next){
-		console.log('Private route hit');
-		next();
-	},
-	logger : function(req,res,next){
-		console.log('Reqeust '+ new Date().toString()+' '+req.method +' '+ req.originalUrl);
-		next();
-	}
-}
+var middleware = require('./middleware.js');
+
 // app.use is used to addd middleware
 // app.use(middleware.requireAuthentication)
 // specifying application level middleware
 app.use(middleware.logger);
 // we can also add middleware to specific route
 app.get('/aboutus',middleware.requireAuthentication,function(req,res){
-	res.send('THis is about us');
+	res.send('This is about us');
 })
 
 // __dirname is provided by the node
